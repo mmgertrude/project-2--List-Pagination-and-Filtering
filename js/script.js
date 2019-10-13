1dd54f2b04b4da20bd3e183f5801be91b8b513b4
mmgertrude <22059601+mmgertrude@users.noreply.github.com>
2019-10-13 13:20:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,70 +2,66 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+
+"use strict"
+const studentsPerPage = 10;
+const studentList = document.querySelectorAll("ul.student-list li");
+
+
+function showPage(studentList, pageNumber){
+   const startIndex = (pageNumber * studentsPerPage) - studentsPerPage;
+   const endIndex = (pageNumber * studentsPerPage) - 1;
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+   for (let i= 0; i< studentList.length; i++){
+      if (i >= startIndex && i <= endIndex ){
+         studentList[i].style.display = "block";
+   }else {studentList[i].style.display = "none";}
+}}
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+function appendPageLinks(studentList){
+   const pagesNeeded = Math.ceil(studentList.length/studentsPerPage);
+   const mainDiv = document.querySelector(".page");
+   let div = document.createElement("div");
+   div.className = "pagination";
+   mainDiv.appendChild(div);
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-   
-***/
+   const ul = document.createElement("ul");
+   div.appendChild(ul);
 
+   for ( let i = 1; i <= pagesNeeded; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.textContent = i;
+      let pageNumber = a.textContent;
+      li.appendChild(a);
+      div.appendChild(li);
 
-/*
-const showPage = (list, page) => {
-/*
-Loop over items in the list parameter
--- If the index of a list item is >= the index of the first
-item that should be shown on the page
--- && the list item index is <= the index of the last item
-that should be shown on the page, show it
+   a.addEventListener('click', (e) => {         
+      showPage(studentList, pageNumber);
+      let activeAncher = document.querySelector('.active');
+      activeAncher.className = "";
+      e.target.className = "active";});
+
+   if (a.textContent === '1') {a.className ="active";}
+
+}
+
+}
+
+showPage(studentList, 1);
+appendPageLinks(studentList);
+   /*
+
+3. Add a ul to the “pagination” div to store the pagination links
+4. for every page, add li and a tags with the page number text
+5. Add an event listener to each a tag. When they are clicked
+call the showPage function to display the appropriate page
+6. Loop over pagination links to remove active class from all links
+7. Add the active class to the link that was just clicked. You can identify that
+clicked link using event.target
 */
 
-
-
-const studentsPerPage = 10;
-let studentList = document.querySelectorAll("ul.student-list li");
-console.log(studentList.length)
-let testStudentList = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]
-
-function showPage(studentsPerPage, testStudentList){
-   testStudentList.forEach(student, index){
-      console.log(student , index)
-   }
-
-}
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-function appendPageLinks(){
-
-}
 
 
 
